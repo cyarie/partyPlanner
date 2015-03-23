c3353b92097f3b453b5ce6136c6bdcfe8a30d256
cyarie <cyarie@gmail.com>
2015-03-23 10:33:49 -0400

    adding these files in for real

--- wedding/views.py
+++ wedding/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render
+from wedding.models import People
 
 
 def index(request):
-    return HttpResponse("Welcome to my eApp!")
+    last_five_people = People.objects.order_by("name")[:5]
+    context = {"last_five_people": last_five_people}
+    return render(request, "wedding/index.html", context)
