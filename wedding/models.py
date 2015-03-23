390307e496de79085b912edabc1459e69c04905d
cyarie <cyarie@gmail.com>
2015-03-22 21:58:27 -0400

    initial commit

---
+++ wedding/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+# Let's make some models! Only really need to write in one model here, for the people we are going to invite.
+class People(models.Model):
+    name = models.CharField(max_length=255)
+    address1 = models.CharField(max_length=255)
+    address2 = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=45)
+    zip_code = models.SmallIntegerField(max_length=5)
+    comment = models.TextField()
+
+    def __unicode__(self):
+        return self.name
