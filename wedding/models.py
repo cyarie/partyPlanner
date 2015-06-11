51082061510fca00decb5a6543c86f02afafa45a
cyarie <cyarie@gmail.com>
2015-06-11 11:44:54 -0400

    Changing the wedding model

--- wedding/models.py
+++ wedding/models.py
@@ -4,14 +4,14 @@ from django.contrib.auth.models import User
 
 # Let's make some models! Only really need to write in one model here, for the people we are going to invite.
 class People(models.Model):
-    name = models.CharField(max_length=255)
-    address1 = models.CharField(max_length=255)
-    address2 = models.CharField(max_length=255)
-    city = models.CharField(max_length=255)
-    state = models.CharField(max_length=45)
-    zip_code = models.SmallIntegerField(max_length=5)
-    email = models.CharField(max_length=255)
-    comment = models.TextField()
+    name = models.CharField(max_length=255, default=None)
+    address1 = models.CharField(max_length=255, default=None)
+    address2 = models.CharField(max_length=255, default=None)
+    city = models.CharField(max_length=255, default=None)
+    state = models.CharField(max_length=45, default=None)
+    zip_code = models.SmallIntegerField(max_length=5, default=None)
+    email = models.CharField(max_length=255, default=None)
+    comment = models.TextField(default=None)
     created_by = models.ForeignKey(User)
     create_dt = models.DateTimeField(auto_now_add=True)
     update_dt = models.DateTimeField(auto_now=True)
